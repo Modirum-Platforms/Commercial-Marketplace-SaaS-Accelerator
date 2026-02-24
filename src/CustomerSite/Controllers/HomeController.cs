8ee79f75f605707826ef45593734c2de5cae1382
Ilija Lazarov <ilija.lazarov@modirumplatforms.com>
2026-02-24 23:12:01 +0200

    Removed logos added by mistake

--- src/CustomerSite/Controllers/HomeController.cs
+++ src/CustomerSite/Controllers/HomeController.cs
@@ -216,8 +216,8 @@ public class HomeController : BaseController
             SubscriptionResult subscriptionDetail = new SubscriptionResult();
             SubscriptionResultExtension subscriptionExtension = new SubscriptionResultExtension();
 
-            this.applicationConfigService.SaveFileToDisk("LogoFile", "mp-logo-light.svg");
-            this.applicationConfigService.SaveFileToDisk("FaviconFile", "favicon.png");
+            this.applicationConfigService.SaveFileToDisk("LogoFile", "contoso-sales.png");
+            this.applicationConfigService.SaveFileToDisk("FaviconFile", "favicon.ico");
 
             if (this.User.Identity.IsAuthenticated)
             {
